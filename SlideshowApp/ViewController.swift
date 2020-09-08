4846d8d5b390e27b0b91d56e61a773bb53316a9f
shuya watanabe <swatanabe990104@gmail.com>
2020-09-08 22:11:57 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by Shuya Watanabe on 2020/09/08.
+//  Copyright © 2020 Shuya Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
